b083ed3fda7cc51556ae7046932f8db4be3ac721
elizasapir <eliza.koren@logmein.com>
2019-06-04 14:48:35 +0300

    support uploaf file

--- BasicSample/BasicSample/ChatViewControllers/FileUploadDemoViewController.swift
+++ BasicSample/BasicSample/ChatViewControllers/FileUploadDemoViewController.swift
@@ -8,12 +8,12 @@ import Foundation
 import OpalImagePicker
 import Photos
 import Bold360AI
-
+import MobileCoreServices
+import HSAttachmentPicker
 
 class FileUploadDemoViewController: AgentViewController {
     
     lazy var imagePicker: UIImagePickerController = { return UIImagePickerController()}()
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,11 +22,35 @@ class FileUploadDemoViewController: AgentViewController {
 
 extension FileUploadDemoViewController: OpalImagePickerControllerDelegate {
     func didClickUploadFile() {
-        //Example instantiating OpalImagePickerController with delegate
-        let imagePicker = OpalImagePickerController()
-        imagePicker.imagePickerDelegate = self
-        imagePicker.maximumSelectionsAllowed = 10
-        self.navigationController?.presentedViewController?.present(imagePicker, animated: true, completion: nil)
+        
+        let actionSheet = UIAlertController(title: "Choose File Selector", message: nil, preferredStyle: .actionSheet)
+        
+        // Create your actions - take a look at different style attributes
+        let reportAction = UIAlertAction(title: "Choose media", style: .default) { (action) in
+            //Example instantiating OpalImagePickerController with delegate
+            let imagePicker = OpalImagePickerController()
+            imagePicker.imagePickerDelegate = self
+            imagePicker.maximumSelectionsAllowed = 10
+            self.navigationController?.presentedViewController?.present(imagePicker, animated: true, completion: nil)
+        }
+        
+        let blockAction = UIAlertAction(title: "Choose File", style: .default) { (action) in
+            let documentPicker: UIDocumentPickerViewController = UIDocumentPickerViewController(documentTypes: ["public.data"], in: UIDocumentPickerMode.import)
+            documentPicker.delegate = self
+            documentPicker.modalPresentationStyle = UIModalPresentationStyle.fullScreen
+            self.navigationController?.presentedViewController?.present(documentPicker, animated: true, completion: nil)
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (action) in
+            print("didPress cancel")
+        }
+        
+        // Add the actions to your actionSheet
+        actionSheet.addAction(reportAction)
+        actionSheet.addAction(blockAction)
+        actionSheet.addAction(cancelAction)
+        // Present the controller
+        self.navigationController?.presentedViewController?.present(actionSheet, animated: true, completion: nil)
     }
     
     func imagePickerDidCancel(_ picker: OpalImagePickerController) {
@@ -39,8 +63,8 @@ extension FileUploadDemoViewController: OpalImagePickerControllerDelegate {
         for image in images {
             if let fileData = image.jpegData(compressionQuality: 1.0) {
                 let request = UploadRequest()
-                request.fileName = "fileName.jpeg"
-                request.fileType = .picture
+                request.fileName = "fileName.mov"
+                request.fileType = .default
                 request.fileData = fileData
                 
                 self.chatController.uploadFile(request) { (info: FileUploadInfo!) in
@@ -51,6 +75,36 @@ extension FileUploadDemoViewController: OpalImagePickerControllerDelegate {
         
         //Dismiss Controller
          self.navigationController?.presentedViewController?.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension FileUploadDemoViewController: UIDocumentMenuDelegate, UIDocumentPickerDelegate {
+    public func documentMenu(_ documentMenu:UIDocumentMenuViewController, didPickDocumentPicker documentPicker: UIDocumentPickerViewController) {
+        documentPicker.delegate = self
+        self.navigationController?.presentedViewController?.present(documentPicker, animated: true, completion: nil)
+    }
+    
+    public func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+        let request = UploadRequest()
+        request.fileName = "fileName.pdf"
+        request.fileType = .default
+        
+        var data: Data!
+        
+        if FileManager.default.fileExists(atPath: urls.first!.path) {
+            data = FileManager.default.contents(atPath: urls.first!.path)
+        }
+        
+        request.fileData = data
+        
+        self.chatController.uploadFile(request) { (info: FileUploadInfo!) in
+            self.chatController.handle(BoldEvent.fileUploaded(info))
+        }
+    }
+    
+    func documentPickerWasCancelled(_ controller: UIDocumentPickerViewController) {
+        print("view was cancelled")
+        dismiss(animated: true, completion: nil)
     }
 }
 
