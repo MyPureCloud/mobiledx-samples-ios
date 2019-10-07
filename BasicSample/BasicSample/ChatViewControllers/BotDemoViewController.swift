f238b75df89e4986d8dbb4b0133b3d8b3869c297
Nissim Pardo <androdogs@gmail.com>
2019-10-07 12:56:07 +0300

    fixed warning for statement scope

--- BasicSample/BasicSample/ChatViewControllers/BotDemoViewController.swift
+++ BasicSample/BasicSample/ChatViewControllers/BotDemoViewController.swift
@@ -13,8 +13,9 @@ class BotDemoViewController: UIViewController {
     
     func createAccount() -> Account {
         let account = BotAccount()
-        account.account = "nanorep"
-        account.knowledgeBase = "English"
+        account.account = "jio"
+        account.knowledgeBase = "Staging_Updated"
+        account.perform(Selector("setServer:"), with: "qa07")
         return account
     }
     
