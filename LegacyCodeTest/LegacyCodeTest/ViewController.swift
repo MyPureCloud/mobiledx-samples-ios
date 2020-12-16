fccd18a6e7073156ca5017a1ef71c3803d3d6f3f
elizasapir <eliza.koren@logmein.com>
2020-12-16 12:43:33 +0200

    add LegacyCodeTest

---
+++ LegacyCodeTest/LegacyCodeTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LegacyCodeTest
+//
+//  Created by Eliza Koren on 12/16/20.
+//
+
+import UIKit
+import BoldEngineUI
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
