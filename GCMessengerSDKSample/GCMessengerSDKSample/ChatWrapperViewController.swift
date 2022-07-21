d29159cfeee6e4a206063939ca6b3bae6f134897
Golan Shoval Gil <golan.shoval_gil@genesys.com>
2022-07-21 15:30:25 +0300

    Spinner added to indicate progress

--- GCMessengerSDKSample/GCMessengerSDKSample/ChatWrapperViewController.swift
+++ GCMessengerSDKSample/GCMessengerSDKSample/ChatWrapperViewController.swift
@@ -8,7 +8,8 @@ import UIKit
 import GenesysCloud
 
 class ChatWrapperViewController: UIViewController {
-
+    let activityView = UIActivityIndicatorView(style: .large)
+    
     var chatController: ChatController!
     var chatVC: UINavigationController!
     var messengerAccount = MessengerAccount()
@@ -18,10 +19,33 @@ class ChatWrapperViewController: UIViewController {
         chatController = ChatController(account: messengerAccount)
         chatController.delegate = self
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        setSpinner()
+    }
 
     @objc func dismissChat(_ sender: UIBarButtonItem?) {
         self.chatController.terminate()
         presentingViewController?.dismiss(animated: true)
+    }
+    
+    func startSpinner() {
+        activityView.startAnimating()
+    }
+    
+    func stopSpinner() {
+        activityView.stopAnimating()
+    }
+    
+    func setSpinner() {
+        activityView.frame = view.frame
+        activityView.layer.backgroundColor = UIColor(white: 0.0, alpha: 0.3).cgColor
+        activityView.center = view.center
+        
+        activityView.hidesWhenStopped = true
+        
+        view.addSubview(activityView)
     }
 }
 
@@ -30,6 +54,7 @@ extension ChatWrapperViewController: ChatControllerDelegate {
         viewController.modalPresentationStyle = .overFullScreen
         self.present(viewController, animated: true) { () -> Void in
             viewController.viewControllers.first?.navigationItem.leftBarButtonItem = UIBarButtonItem(title: "End Chat", style: .plain, target: self, action: #selector(ChatWrapperViewController.dismissChat(_:)))
+            
         }
     }
 
@@ -40,6 +65,21 @@ extension ChatWrapperViewController: ChatControllerDelegate {
         }))
         present(alert, animated: true)
     }
+    
+    func didUpdateState(_ event: ChatStateEvent!) {
+        print("Chat state: \(event.state)")
+        
+        switch event.state {
+        case .preparing:
+            print("preparing")
+            startSpinner()
+        case .started:
+            print("started")
+        default:
+            print(event.state)
+        }
+    }
+    
 }
 
 
