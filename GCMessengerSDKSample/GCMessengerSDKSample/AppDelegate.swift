443ea4a772afb5b92f29b7d53117d84959c69d31
Eliza Koren <eliza.koren@logmein.com>
2023-03-26 20:44:51 +0300

    [GMDX-1325] add crashlytics with condition

--- GCMessengerSDKSample/GCMessengerSDKSample/AppDelegate.swift
+++ GCMessengerSDKSample/GCMessengerSDKSample/AppDelegate.swift
@@ -5,12 +5,16 @@
 // ===================================================================================================
 
 import UIKit
+import FirebaseCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        if (Bundle.main.path(forResource: "GoogleService-Info", ofType: "plist") != nil) {
+            FirebaseApp.configure()
+        }
         // Override point for customization after application launch.
         return true
     }
