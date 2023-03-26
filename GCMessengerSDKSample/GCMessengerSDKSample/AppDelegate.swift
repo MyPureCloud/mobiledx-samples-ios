ed6b76881f364599f7095cc4ace32306a67ccc21
Eliza Koren <eliza.koren@logmein.com>
2023-03-26 20:47:10 +0300

    [GMDX-1325] add log when Google Services & Crashlytics enabled

--- GCMessengerSDKSample/GCMessengerSDKSample/AppDelegate.swift
+++ GCMessengerSDKSample/GCMessengerSDKSample/AppDelegate.swift
@@ -13,6 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         if (Bundle.main.path(forResource: "GoogleService-Info", ofType: "plist") != nil) {
+            print("Google Services & Crashlytics enabled")
             FirebaseApp.configure()
         }
         // Override point for customization after application launch.
