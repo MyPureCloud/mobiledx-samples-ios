7cc156a36ea1181cf8feb4bb86f92b9c6f02801c
Eliza Koren <elizasapir@gmail.com>
2023-07-22 20:49:29 +0300

    align beta 1-3 with master

--- GCMessengerSDKSample/GCMessengerSDKSample/AccountDetailsViewController.swift
+++ GCMessengerSDKSample/GCMessengerSDKSample/AccountDetailsViewController.swift
@@ -53,7 +53,6 @@ class AccountDetailsViewController: UIViewController {
         } else {
             let account = MessengerAccount(deploymentId: deploymentIdTextField.text ?? "",
                                            domain: domainIdTextField.text ?? "",
-                                           tokenStoreKey: "",
                                            logging: loggingSwitch.isOn)
             
             updateUserDefaults()
