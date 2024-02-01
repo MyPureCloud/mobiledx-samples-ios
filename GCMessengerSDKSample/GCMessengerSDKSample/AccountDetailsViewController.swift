b5760fc53af7c02f1f71fd0f63364676be401cec
Shahar Wienrib <shahar.wienrib@genesys.com>
2024-02-01 15:35:14 +0200

    Use logger

--- GCMessengerSDKSample/GCMessengerSDKSample/AccountDetailsViewController.swift
+++ GCMessengerSDKSample/GCMessengerSDKSample/AccountDetailsViewController.swift
@@ -9,20 +9,24 @@ import UIKit
 import GenesysCloud
 import GenesysCloudMessenger
 
-class AccountDetailsViewController: UIViewController {
+class AccountDetailsViewController: UIViewController, MFMailComposeViewControllerDelegate {
     @IBOutlet weak var deploymentIdTextField: UITextField!
     @IBOutlet weak var domainIdTextField: UITextField!
     @IBOutlet weak var customAttributesTextField: UITextField!
     @IBOutlet weak var startChatButton: UIButton!
     @IBOutlet weak var loggingSwitch: UISwitch!
     @IBOutlet weak var versionAndBuildLabel: UILabel!
+    @IBOutlet weak var sendLogButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupFields()
-
+        
         let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
         view.addGestureRecognizer(tap)
+        
+        let longPressRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(longPressed))
+            self.sendLogButton.addGestureRecognizer(longPressRecognizer)
         
         if let versionNumber = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String,
            let buildNumber = Bundle.main.infoDictionary?["CFBundleVersion"] as? String {
@@ -38,7 +42,16 @@ class AccountDetailsViewController: UIViewController {
             }
         }
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let pathLog = "\(Logger.defaultDirectory())/GenesysMobileSDK_LOG.log"
+        if let _ = NSData(contentsOfFile: pathLog) {
+            sendLogButton.isHidden = false
+        }
+    }
+    
     @objc func textFieldDidChange(_ textField: UITextField) {
         if let deploymentId = deploymentIdTextField.text, let domainId = domainIdTextField.text {
             startChatButton.isEnabled = !deploymentId.isEmpty && !domainId.isEmpty
@@ -82,8 +95,6 @@ class AccountDetailsViewController: UIViewController {
     }
     
     private func createAccountForValidInputFields() -> MessengerAccount? {
-
-        
         guard checkInputFieldIsValid(deploymentIdTextField) || checkInputFieldIsValid(domainIdTextField) else {
             showErrorAlert(message: "One or more required fields needed, please check & try again")
             return nil
@@ -106,9 +117,8 @@ class AccountDetailsViewController: UIViewController {
         }
         
         updateUserDefaults()
-
+        
         return account
-
     }
     
     private func showErrorAlert(message: String) {
@@ -120,7 +130,7 @@ class AccountDetailsViewController: UIViewController {
     private func markInvalidTextField(_ requiredTextField: UITextField) {
         requiredTextField.isError(baseColor: UIColor.red.cgColor, numberOfShakes: 3, revert: true)
     }
-        
+    
     private func updateUserDefaults() {
         UserDefaults.deploymentId = deploymentIdTextField.text ?? ""
         UserDefaults.domainId = domainIdTextField.text ?? ""
@@ -133,5 +143,53 @@ class AccountDetailsViewController: UIViewController {
         controller.messengerAccount = account
         controller.modalPresentationCapturesStatusBarAppearance = true
         present(controller, animated: true)
+    }
+    
+    @IBAction func onSendLogButtonClicked(_ sender: Any) {
+        if MFMailComposeViewController.canSendMail() {
+            let mail = MFMailComposeViewController()
+            mail.mailComposeDelegate = self
+            mail.setToRecipients(["shahar.wienrib@genesys.com"])
+            
+            let systemName: String = UIDevice.current.systemName
+            let systemVersion: String = UIDevice.current.systemVersion
+            
+            mail.setSubject("GenesysMobileSDK, \(systemName) \(systemVersion), \(getAppVersionAndBuildNumber())")
+            mail.setMessageBody("See attached log file:", isHTML: true)
+            
+            let pathLog = "\(Logger.defaultDirectory())/GenesysMobileSDK_LOG.log"
+            
+            if let fileData = NSData(contentsOfFile: pathLog) {
+                mail.addAttachmentData(fileData as Data, mimeType: "application/text", fileName: "GenesysMobileSDK_LOG.log")
+            }
+            
+            present(mail, animated: true)
+        } else {
+            printLog("Can't send email")
+            ToastManager.shared.showToast(message: "Can't send email")
+        }
+    }
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true)
+    }
+    
+    func getAppVersionAndBuildNumber() -> String {
+        var text: String = ""
+        if let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String {
+            text += "App Version: " + version
+        }
+        
+        if let buildNumber = Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as? String {
+            text += ", "
+            text += "Build Number: " + buildNumber
+        }
+        
+        return text
+    }
+    
+    @objc func longPressed(sender: UILongPressGestureRecognizer) {
+        Logger.logger.clearLogFile()
+        ToastManager.shared.showToast(message: "Log cleared")
     }
 }
