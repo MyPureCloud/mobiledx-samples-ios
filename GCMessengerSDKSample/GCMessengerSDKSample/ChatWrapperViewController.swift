958929a893757ece38d4cde5619f7b586a27bf06
Golan Shoval Gil <golan.shoval_gil@genesys.com>
2025-01-05 13:55:19 +0200

    [GMMS-6934] SCP - Handle Attachment Validation Failure state

--- GCMessengerSDKSample/GCMessengerSDKSample/ChatWrapperViewController.swift
+++ GCMessengerSDKSample/GCMessengerSDKSample/ChatWrapperViewController.swift
@@ -178,6 +178,12 @@ extension ChatWrapperViewController: ChatControllerDelegate {
                 if let errorDescription = error.errorDescription {
                     ToastManager.shared.showToast(message: errorDescription)
                 }
+                
+            case .attachmentValidationError:
+                print("** Error: \(error.errorType.rawValue)")
+                if let errorDescription = error.errorDescription {
+                    showAuthenticatedSessionErrorAlert(message: errorDescription)
+                }
 
             default:
                 break
