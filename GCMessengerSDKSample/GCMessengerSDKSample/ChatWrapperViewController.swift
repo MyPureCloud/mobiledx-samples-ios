6df992523861331c4dd1e1690a6658fce4c9fd24
Shahar Wienrib <shahar.wienrib@genesys.com>
2025-02-11 11:25:53 +0200

    [GMMS-7472] Handle .Close event with ended reason Clear Conversation

--- GCMessengerSDKSample/GCMessengerSDKSample/ChatWrapperViewController.swift
+++ GCMessengerSDKSample/GCMessengerSDKSample/ChatWrapperViewController.swift
@@ -261,12 +261,14 @@ extension ChatWrapperViewController: ChatControllerDelegate {
                     switch endedReason {
                     case EndedReason.sessionLimitReached:
                         ToastManager.shared.showToast(message: "You have been logged out because the session limit was exceeded.")
+                        presentingViewController?.dismiss(animated: true)
+                    case EndedReason.conversationCleared:
+                        ToastManager.shared.showToast(message: "Conversation was cleared.")
                     default:
                         break
                     }
                 }
                 
-                presentingViewController?.dismiss(animated: true)
             default:
                 print(event.state)
             }
